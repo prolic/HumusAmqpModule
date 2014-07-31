c5f86f3a597c78d51b109403cc63a33e87183e9c
prolic <saschaprolic@googlemail.com>
2014-07-31 17:09:22 +0200

    bugfix in setup fabric controller

--- src/HumusAmqpModule/Controller/SetupFabricController.php
+++ src/HumusAmqpModule/Controller/SetupFabricController.php
@@ -54,16 +54,18 @@ class SetupFabricController extends AbstractConsoleController
 
         $this->console->writeLine('Setting up the AMQP fabric');
 
+        $that = $this;
+
         array_map(
-            function ($name) {
-                if ($this->partsHolder->hasParts($name)) {
-                    $this->console->write('Declaring exchanges and queues for ' . $name . ' ');
-                    foreach ($this->partsHolder->getParts($name) as $part) {
+            function ($name) use ($that)  {
+                if ($that->partsHolder->hasParts($name)) {
+                    $that->console->write('Declaring exchanges and queues for ' . $name . ' ');
+                    foreach ($that->partsHolder->getParts($name) as $part) {
                         $part->setupFabric();
                     }
-                    $this->console->writeLine('OK', ColorInterface::GREEN);
+                    $that->console->writeLine('OK', ColorInterface::GREEN);
                 } else {
-                    $this->console->writeLine('No ' . $name . ' found to configure', ColorInterface::YELLOW);
+                    $that->console->writeLine('No ' . $name . ' found to configure', ColorInterface::YELLOW);
                 }
             },
             array(
