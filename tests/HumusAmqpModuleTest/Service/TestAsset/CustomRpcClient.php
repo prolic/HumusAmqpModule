8d0bbb687fa71e6b7f77f224622734fb70a30c21
prolic <saschaprolic@googlemail.com>
2014-08-06 14:13:51 +0200

    add test assets

---
+++ tests/HumusAmqpModuleTest/Service/TestAsset/CustomRpcClient.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace HumusAmqpModuleTest\Service\TestAsset;
+
+use HumusAmqpModule\Amqp\RpcClient;
+
+class CustomRpcClient extends RpcClient
+{
+}
---
+++ tests/HumusAmqpModuleTest/Service/TestAsset/CustomRpcServer.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace HumusAmqpModuleTest\Service\TestAsset;
+
+use HumusAmqpModule\Amqp\RpcServer;
+
+class CustomRpcServer extends RpcServer
+{
+}
