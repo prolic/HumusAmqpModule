b1bd7b5d568da36c71202eaa5285e79695d99372
prolic <saschaprolic@googlemail.com>
2014-08-24 17:21:09 +0200

    delete connection extension and abstract connection aware service factory

---
+++ src/HumusAmqpModule/QueueFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace HumusAmqpModule;
+
+use AMQPChannel;
+use AMQPExchange;
+
+class ExchangeFactory
+{
+    /**
+     * @param ExchangeSpecification $specification
+     * @param AMQPChannel $channel
+     * @param bool $autoDeclare
+     * @return AMQPExchange
+     */
+    public function create(ExchangeSpecification $specification, AMQPChannel $channel, $autoDeclare = true)
+    {
+        $exchange = new AMQPExchange($channel);
+        $exchange->setType($specification->getType()->getValue());
+        $exchange->setFlags($specification->getFlags());
+        $exchange->setArguments($specification->getArguments());
+
+        if ($autoDeclare) {
+            $exchange->declareExchange();
+
+            // rabbitmq extension: exchange to exchange bindings
+            foreach ($specification->getExchangeBindings() as $exchangeName => $routingKeys) {
+                foreach ($routingKeys as $routingKey) {
+                    $exchange->bind($exchangeName, $routingKey, $specification->getFlags());
+                }
+            }
+        }
+
+        return $exchange;
+    }
+}
