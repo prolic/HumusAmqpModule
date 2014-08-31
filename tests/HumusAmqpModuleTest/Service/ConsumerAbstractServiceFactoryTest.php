44d0ed4fba33415b14aefaa77d57f62cd95dcf4c
prolic <saschaprolic@googlemail.com>
2014-08-31 19:55:20 +0200

    add consumer abstract service factory tests for exception cases

--- tests/HumusAmqpModuleTest/Service/ConsumerAbstractServiceFactoryTest.php
+++ tests/HumusAmqpModuleTest/Service/ConsumerAbstractServiceFactoryTest.php
@@ -36,37 +36,8 @@ class ConsumerAbstractServiceFactoryTest extends \PHPUnit_Framework_TestCase
      */
     protected $components;
 
-    public function setUp()
+    public function prepare($config)
     {
-        $config = array(
-            'humus_amqp_module' => array(
-                'default_connection' => 'default',
-                'exchanges' => array(
-                    'demo-exchange' => array(
-                        'name' => 'demo-exchange',
-                        'type' => 'direct'
-                    )
-                ),
-                'queues' => array(
-                    'demo-queue' => array(
-                        'name' => 'demo-queue',
-                        'exchange' => 'demo-exchange'
-                    )
-                ),
-                'consumers' => array(
-                    'test-consumer' => array(
-                        'connection' => 'default',
-                        'queues' => ['demo-queue'],
-                        'auto_setup_fabric' => false,
-                        'callback' => 'test-callback',
-                        'qos' => array(
-                            'prefetchCount' => 10
-                        )
-                    ),
-                ),
-            )
-        );
-
         $connection = $this->getMock('AMQPConnection', array(), array(), '', false);
         $channel    = $this->getMock('AMQPChannel', array(), array(), '', false);
         $channel
@@ -113,9 +84,446 @@ class ConsumerAbstractServiceFactoryTest extends \PHPUnit_Framework_TestCase
 
     public function testCreateConsumer()
     {
+        $config = array(
+            'humus_amqp_module' => array(
+                'default_connection' => 'default',
+                'exchanges' => array(
+                    'demo-exchange' => array(
+                        'name' => 'demo-exchange',
+                        'type' => 'direct'
+                    )
+                ),
+                'queues' => array(
+                    'demo-queue' => array(
+                        'name' => 'demo-queue',
+                        'exchange' => 'demo-exchange'
+                    )
+                ),
+                'consumers' => array(
+                    'test-consumer' => array(
+                        'connection' => 'default',
+                        'queues' => ['demo-queue'],
+                        'auto_setup_fabric' => false,
+                        'callback' => 'test-callback',
+                        'qos' => array(
+                            'prefetchCount' => 10
+                        )
+                    ),
+                ),
+            )
+        );
+
+        $this->prepare($config);
+
         $consumer = $this->components->createServiceWithName($this->services, 'test-consumer', 'test-consumer');
         $consumer2 = $this->components->createServiceWithName($this->services, 'test-consumer', 'test-consumer');
         $this->assertNotSame($consumer, $consumer2);
         $this->assertInstanceOf('HumusAmqpModule\Consumer', $consumer);
+    }
+
+    /**
+     * @expectedException HumusAmqpModule\Exception\InvalidArgumentException
+     * @expectedExceptionMessage The logger invalid stuff is not configured
+     */
+    public function testCreateConsumerThrowsExceptionOnInvalidLogger()
+    {
+        $config = array(
+            'humus_amqp_module' => array(
+                'default_connection' => 'default',
+                'exchanges' => array(
+                    'demo-exchange' => array(
+                        'name' => 'demo-exchange',
+                        'type' => 'direct'
+                    )
+                ),
+                'queues' => array(
+                    'demo-queue' => array(
+                        'name' => 'demo-queue',
+                        'exchange' => 'demo-exchange'
+                    )
+                ),
+                'consumers' => array(
+                    'test-consumer' => array(
+                        'connection' => 'default',
+                        'queues' => ['demo-queue'],
+                        'auto_setup_fabric' => false,
+                        'logger' => 'invalid stuff',
+                        'callback' => 'test-callback',
+                        'qos' => array(
+                            'prefetchCount' => 10
+                        )
+                    ),
+                ),
+            )
+        );
+
+        $this->prepare($config);
+
+        $this->components->createServiceWithName($this->services, 'test-consumer', 'test-consumer');
+    }
+
+    /**
+     * @expectedException HumusAmqpModule\Exception\InvalidArgumentException
+     * @expectedExceptionMessage The required callback invalid-callback can not be found
+     */
+    public function testCreateConsumerThrowsExceptionOnInvalidCallback()
+    {
+        $config = array(
+            'humus_amqp_module' => array(
+                'default_connection' => 'default',
+                'exchanges' => array(
+                    'demo-exchange' => array(
+                        'name' => 'demo-exchange',
+                        'type' => 'direct'
+                    )
+                ),
+                'queues' => array(
+                    'demo-queue' => array(
+                        'name' => 'demo-queue',
+                        'exchange' => 'demo-exchange'
+                    )
+                ),
+                'consumers' => array(
+                    'test-consumer' => array(
+                        'connection' => 'default',
+                        'queues' => ['demo-queue'],
+                        'auto_setup_fabric' => false,
+                        'callback' => 'invalid-callback',
+                        'qos' => array(
+                            'prefetchCount' => 10
+                        )
+                    ),
+                ),
+            )
+        );
+
+        $this->prepare($config);
+
+        $this->components->createServiceWithName($this->services, 'test-consumer', 'test-consumer');
+    }
+
+    /**
+     * @expectedException HumusAmqpModule\Exception\InvalidArgumentException
+     * @expectedExceptionMessage The required callback invalid-callback can not be found
+     */
+    public function testCreateConsumerThrowsExceptionOnInvalidFlushCallback()
+    {
+        $config = array(
+            'humus_amqp_module' => array(
+                'default_connection' => 'default',
+                'exchanges' => array(
+                    'demo-exchange' => array(
+                        'name' => 'demo-exchange',
+                        'type' => 'direct'
+                    )
+                ),
+                'queues' => array(
+                    'demo-queue' => array(
+                        'name' => 'demo-queue',
+                        'exchange' => 'demo-exchange'
+                    )
+                ),
+                'consumers' => array(
+                    'test-consumer' => array(
+                        'connection' => 'default',
+                        'queues' => ['demo-queue'],
+                        'auto_setup_fabric' => false,
+                        'callback' => 'test-callback',
+                        'flush_callback' => 'invalid-callback',
+                        'qos' => array(
+                            'prefetchCount' => 10
+                        )
+                    ),
+                ),
+            )
+        );
+
+        $this->prepare($config);
+
+        $this->components->createServiceWithName($this->services, 'test-consumer', 'test-consumer');
+    }
+
+    /**
+     * @expectedException HumusAmqpModule\Exception\InvalidArgumentException
+     * @expectedExceptionMessage The required callback invalid-callback can not be found
+     */
+    public function testCreateConsumerThrowsExceptionOnInvalidErrorCallback()
+    {
+        $config = array(
+            'humus_amqp_module' => array(
+                'default_connection' => 'default',
+                'exchanges' => array(
+                    'demo-exchange' => array(
+                        'name' => 'demo-exchange',
+                        'type' => 'direct'
+                    )
+                ),
+                'queues' => array(
+                    'demo-queue' => array(
+                        'name' => 'demo-queue',
+                        'exchange' => 'demo-exchange'
+                    )
+                ),
+                'consumers' => array(
+                    'test-consumer' => array(
+                        'connection' => 'default',
+                        'queues' => ['demo-queue'],
+                        'auto_setup_fabric' => false,
+                        'callback' => 'test-callback',
+                        'error_callback' => 'invalid-callback',
+                        'qos' => array(
+                            'prefetchCount' => 10
+                        )
+                    ),
+                ),
+            )
+        );
+
+        $this->prepare($config);
+
+        $this->components->createServiceWithName($this->services, 'test-consumer', 'test-consumer');
+    }
+
+    /**
+     * @expectedException HumusAmqpModule\Exception\InvalidArgumentException
+     * @expectedExceptionMessage Queues are missing for consumer test-consumer
+     */
+    public function testCreateConsumerThrowsExceptionOnMissingQueues()
+    {
+        $config = array(
+            'humus_amqp_module' => array(
+                'default_connection' => 'default',
+                'exchanges' => array(
+                    'demo-exchange' => array(
+                        'name' => 'demo-exchange',
+                        'type' => 'direct'
+                    )
+                ),
+                'queues' => array(
+                    'demo-queue' => array(
+                        'name' => 'demo-queue',
+                        'exchange' => 'demo-exchange'
+                    )
+                ),
+                'consumers' => array(
+                    'test-consumer' => array(
+                        'connection' => 'default',
+                        'auto_setup_fabric' => false,
+                        'callback' => 'test-callback',
+                        'error_callback' => 'invalid-callback',
+                        'qos' => array(
+                            'prefetchCount' => 10
+                        )
+                    ),
+                ),
+            )
+        );
+
+        $this->prepare($config);
+
+        $this->components->createServiceWithName($this->services, 'test-consumer', 'test-consumer');
+    }
+
+    /**
+     * @expectedException HumusAmqpModule\Exception\InvalidArgumentException
+     * @expectedExceptionMessage No delivery callback specified for consumer test-consumer
+     */
+    public function testCreateConsumerThrowsExceptionOnMissingCallback()
+    {
+        $config = array(
+            'humus_amqp_module' => array(
+                'default_connection' => 'default',
+                'exchanges' => array(
+                    'demo-exchange' => array(
+                        'name' => 'demo-exchange',
+                        'type' => 'direct'
+                    )
+                ),
+                'queues' => array(
+                    'demo-queue' => array(
+                        'name' => 'demo-queue',
+                        'exchange' => 'demo-exchange'
+                    )
+                ),
+                'consumers' => array(
+                    'test-consumer' => array(
+                        'connection' => 'default',
+                        'queues' => ['demo-queue'],
+                        'auto_setup_fabric' => false,
+                        'error_callback' => 'invalid-callback',
+                        'qos' => array(
+                            'prefetchCount' => 10
+                        )
+                    ),
+                ),
+            )
+        );
+
+        $this->prepare($config);
+
+        $this->components->createServiceWithName($this->services, 'test-consumer', 'test-consumer');
+    }
+
+    /**
+     * @expectedException HumusAmqpModule\Exception\InvalidArgumentException
+     * @expectedExceptionMessage Queue invalid-queue is missing in the queue configuration
+     */
+    public function testCreateConsumerThrowsExceptionOnMissingQueue()
+    {
+        $config = array(
+            'humus_amqp_module' => array(
+                'default_connection' => 'default',
+                'exchanges' => array(
+                    'demo-exchange' => array(
+                        'name' => 'demo-exchange',
+                        'type' => 'direct'
+                    )
+                ),
+                'queues' => array(
+                    'demo-queue' => array(
+                        'name' => 'demo-queue',
+                        'exchange' => 'demo-exchange'
+                    )
+                ),
+                'consumers' => array(
+                    'test-consumer' => array(
+                        'connection' => 'default',
+                        'queues' => ['invalid-queue'],
+                        'auto_setup_fabric' => false,
+                        'callback' => 'test-callback',
+                        'qos' => array(
+                            'prefetchCount' => 10
+                        )
+                    ),
+                ),
+            )
+        );
+
+        $this->prepare($config);
+
+        $this->components->createServiceWithName($this->services, 'test-consumer', 'test-consumer');
+    }
+
+    /**
+     * @expectedException HumusAmqpModule\Exception\InvalidArgumentException
+     * @expectedExceptionMessage The queues exchange demo-exchange is missing in the exchanges configuration
+     */
+    public function testCreateConsumerThrowsExceptionOnQueuesExchangeMissing()
+    {
+        $config = array(
+            'humus_amqp_module' => array(
+                'default_connection' => 'default',
+                'exchanges' => array(
+                ),
+                'queues' => array(
+                    'demo-queue' => array(
+                        'name' => 'demo-queue',
+                        'exchange' => 'demo-exchange'
+                    )
+                ),
+                'consumers' => array(
+                    'test-consumer' => array(
+                        'connection' => 'default',
+                        'queues' => ['demo-queue'],
+                        'auto_setup_fabric' => false,
+                        'callback' => 'test-callback',
+                        'qos' => array(
+                            'prefetchCount' => 10
+                        )
+                    ),
+                ),
+            )
+        );
+
+        $this->prepare($config);
+
+        $this->components->createServiceWithName($this->services, 'test-consumer', 'test-consumer');
+    }
+
+    public function testCreateConsumerThrowsExceptionOnQueueConnectionMismatch()
+    {
+        $config = array(
+            'humus_amqp_module' => array(
+                'default_connection' => 'default',
+                'exchanges' => array(
+                    'demo-exchange' => array(
+                        'name' => 'demo-exchange',
+                        'type' => 'direct'
+                    )
+                ),
+                'queues' => array(
+                    'demo-queue' => array(
+                        'name' => 'demo-queue',
+                        'exchange' => 'demo-exchange',
+                        'connection' => 'other'
+                    )
+                ),
+                'consumers' => array(
+                    'test-consumer' => array(
+                        'connection' => 'default',
+                        'queues' => ['demo-queue'],
+                        'auto_setup_fabric' => false,
+                        'callback' => 'test-callback',
+                        'qos' => array(
+                            'prefetchCount' => 10
+                        )
+                    ),
+                ),
+            )
+        );
+
+        $this->prepare($config);
+
+        $this->setExpectedException(
+            'HumusAmqpModule\Exception\InvalidArgumentException',
+            'The queue connection for queue demo-queue (other) does not match the consumer '
+            . 'connection for consumer test-consumer (default)'
+        );
+
+        $this->components->createServiceWithName($this->services, 'test-consumer', 'test-consumer');
+    }
+
+    public function testCreateConsumerThrowsExceptionOnQueueToExchangeConnectionMismatch()
+    {
+        $config = array(
+            'humus_amqp_module' => array(
+                'default_connection' => 'default',
+                'exchanges' => array(
+                    'demo-exchange' => array(
+                        'name' => 'demo-exchange',
+                        'type' => 'direct',
+                        'connection' => 'other'
+                    )
+                ),
+                'queues' => array(
+                    'demo-queue' => array(
+                        'name' => 'demo-queue',
+                        'exchange' => 'demo-exchange',
+                        'connection' => 'default'
+                    )
+                ),
+                'consumers' => array(
+                    'test-consumer' => array(
+                        'connection' => 'default',
+                        'queues' => ['demo-queue'],
+                        'auto_setup_fabric' => false,
+                        'callback' => 'test-callback',
+                        'qos' => array(
+                            'prefetchCount' => 10
+                        )
+                    ),
+                ),
+            )
+        );
+
+        $this->prepare($config);
+
+        $this->setExpectedException(
+            'HumusAmqpModule\Exception\InvalidArgumentException',
+            'The exchange connection for exchange demo-exchange (other) does not match the '
+            . 'consumer connection for consumer test-consumer (default)'
+        );
+
+        $this->components->createServiceWithName($this->services, 'test-consumer', 'test-consumer');
     }
 }
