e6ac52dd5c5d0d235c78cee33c9ed749f4472d61
prolic <saschaprolic@googlemail.com>
2014-08-31 19:02:53 +0200

    add queue- & exchange factory interfaces

---
+++ src/HumusAmqpModule/ExchangeFactoryInterface.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace HumusAmqpModule;
+
+use AMQPChannel;
+use AMQPExchange;
+
+class ExchangeFactory
+{
+    /**
+     * @param ExchangeSpecification $specification
+     * @param AMQPChannel $channel
+     * @param bool $autoDeclare
+     * @return AMQPExchange
+     */
+    public function create(ExchangeSpecification $specification, AMQPChannel $channel, $autoDeclare = true)
+    {
+        $exchange = new AMQPExchange($channel);
+        $exchange->setArguments($specification->getArguments());
+        $exchange->setName($specification->getName());
+        $exchange->setFlags($specification->getFlags());
+        $exchange->setType($specification->getType());
+
+        if ($autoDeclare) {
+            $exchange->declareExchange();
+
+            // rabbitmq extension: exchange to exchange bindings
+            foreach ($specification->getExchangeBindings() as $exchangeName => $routingKeys) {
+                foreach ($routingKeys as $routingKey) {
+                    $exchange->bind($exchangeName, $routingKey, $specification->getFlags());
+                }
+            }
+        }
+
+        return $exchange;
+    }
+}
---
+++ src/HumusAmqpModule/QueueFactoryInterface.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace HumusAmqpModule;
+
+use AMQPChannel;
+use AMQPExchange;
+use AMQPQueue;
+
+class QueueFactory
+{
+    /**
+     * @param QueueSpecification $specification
+     * @param AMQPChannel $channel
+     * @param bool $autoDeclare
+     * @return AMQPQueue
+     */
+    public function create(QueueSpecification $specification, AMQPChannel $channel, $autoDeclare = true)
+    {
+        $queue = new AMQPQueue($channel);
+        if ($specification->getName() != '') {
+            $queue->setName($specification->getName());
+        }
+        $queue->setFlags($specification->getFlags());
+        $queue->setArguments($specification->getArguments());
+
+        if ($autoDeclare) {
+            $queue->declareQueue();
+
+            $routingKeys = $specification->getRoutingKeys();
+            if (empty($routingKeys)) {
+                $queue->bind($specification->getExchange(), null, $specification->getBindArguments());
+            } else {
+                foreach ($routingKeys as $routingKey) {
+                    $queue->bind($specification->getExchange(), $routingKey, $specification->getBindArguments());
+                }
+            }
+        }
+
+        return $queue;
+    }
+}
